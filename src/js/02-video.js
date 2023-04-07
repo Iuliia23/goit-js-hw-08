be0fb30e5ec37f6b7bb506dd1b428d9a638182a1
Iuliia23 <117689978+Iuliia23@users.noreply.github.com>
2023-04-07 11:19:09 +0200

    update
    update

---
+++ src/js/02-video.js
@@ -0,0 +1,17 @@
+// import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(time) {
+  localStorage.setItem('videoplayer-current-time', time.seconds);
+}
+const lastPayerTime = localStorage.getItem('videoplayer-current-time');
+if (lastPayerTime) {
+    player.setCurrentTime(lastPayerTime);
+  } else {
+    console.log('=)');
+  }
