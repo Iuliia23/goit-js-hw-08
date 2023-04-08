5ba93f43c84057b929be1cac2af8bc5d691561c7
Iuliia23 <117689978+Iuliia23@users.noreply.github.com>
2023-04-08 16:27:45 +0000

    Deploying to gh-pages from @ Iuliia23/goit-js-hw-08@30806c3016fa6103e6f4e2a5a179bce9987a3fac 🚀

--- 02-video.923d3110.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("#vimeo-player"),t=new Player(e);t.on("timeupdate",throttle((function(e){localStorage.setItem("videoplayer-current-time",e.seconds)}),1e3));const o=localStorage.getItem("videoplayer-current-time");o?t.setCurrentTime(o):console.log("=)");
-//# sourceMappingURL=02-video.923d3110.js.map
