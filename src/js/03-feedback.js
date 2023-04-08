30806c3016fa6103e6f4e2a5a179bce9987a3fac
Iuliia23 <117689978+Iuliia23@users.noreply.github.com>
2023-04-08 18:27:20 +0200

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector(".feedback-form");
+const FORM_KEY = "feedback-form-state";
+
+const onFormInput = () => {
+    const formData = new FormData(feedbackForm);
+    let userForm = {};
+    formData.forEach((value, name) => userForm[name] = value.trim());
+    localStorage.setItem(FORM_KEY, JSON.stringify(userForm));
+};
+
+feedbackForm.addEventListener("input", throttle(onFormInput, 500));
+
+const onPopulateForm = () => {
+    if (localStorage.getItem(FORM_KEY)) {
+        Object.entries(JSON.parse(localStorage.getItem(FORM_KEY))).forEach(([name, value]) => feedbackForm.elements[name].value = value);    }
+};
+
+onPopulateForm();
+
+const onFormSubmit = event => {
+    event.preventDefault();
+    if (feedbackForm.elements.email.value && feedbackForm.elements.message.value !== "") {
+        console.log('Відправляємо форму з даними: ', JSON.parse(localStorage.getItem(FORM_KEY)));
+        event.currentTarget.reset();
+        localStorage.removeItem(FORM_KEY);
+    };
+};
+  
+feedbackForm.addEventListener("submit", onFormSubmit);
